6ee95af6b770c09f98aeefe1b9d58179867f94e3
kjfsoul <KJFSOUL@GMAIL.COM>
2024-12-31 07:18:39 +0000

    client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from './types';
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from './types'
 
-const supabaseUrl = 'https://viwtxbfmkziochmtmfnt.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpd3R4YmZta3ppb2NobXRtZm50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4MjU2MDAsImV4cCI6MjAxOTQwMTYwMH0.GE-B1-6i1LHJHsxjYLkqGTYEKVEFpDqbhWAF0VB9QYs';
+const supabaseUrl = 'https://viwtxbfmkziochmtmfnt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpd3R4YmZta3ppb2NobXRtZm50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2OTU1NDYsImV4cCI6MjA1MDI3MTU0Nn0.BNjkKVLP2AbH2KxRh2LjDuZ_4nkzchGZMYkqiDclvQ4'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
